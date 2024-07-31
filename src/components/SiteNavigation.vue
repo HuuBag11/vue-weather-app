82f7531ef8e2dae05525e17aed74e304283f0929
unknown <ryo.bang@namtech.sg>
2024-07-31 15:33:22 +0700

    first commit

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="sticky top-0 bg-color-primary shadow-lg">
+    <nav
+      class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
+    >
+      <RouterLink :to="{name: 'home'}">
+        <div class="flex items-center gap-3 flex-1">
+          <i class="fa-solid fa-sun text-2xl"></i>
+          <p>The Local Weather</p>
+        </div>
+      </RouterLink>
+
+      <div class="flex gap-3 flex-1 justify-end">
+        <i class="fa-solid fa-circle-info text-xl hover:text-color-secondary duration-150 cursor-pointer"></i>
+        <i class="fa-solid fa-plus text-xl hover:text-color-secondary duration-150 cursor-pointer"></i>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { RouterLink, useRoute, useRouter } from "vue-router";
+
+</script>
